6aa1086162c2f8ff5d835c0ba516c422343cf046
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-02-16 21:59:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname=document.getElementById("fname")
+	let lname=document.getElementById("lname")
+	let btn=document.getElementById("submit")
+	btn.addEventListener("click",alert)
+	function alert(e)
+		{
+			let first=fname.innerText
+			let last=lname.innerText
+			alert(first,last)
+}
 }
